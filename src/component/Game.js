ce875e840a42af315c80ed58beccd979372aca50
HaoYi <karenhaoyitu@gmail.com>
2019-03-29 17:54:54 +0800

    complete the shuffule feature

--- src/component/Game.js
+++ src/component/Game.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 
 class StatusMenu extends React.Component{
+    clickHandler(e){
+        // console.log(this.props.tileClick)
+        // this.props.tileClick(e.target, this.props.position, this.props.status);
+        this.props.restart();
+    }
     render(){
         return (
-            <div className="statusmenu row">
-               
-                <p>[place holders] (counts) clicks / [Win] Puzzle Solved in (counts) clicks</p>
-                
+            <div id="menu"className="statusmenu row center">
+                        <h3 id="subtitle">{this.props.status}</h3>
+                        <button className={this.props.winClass}
+                                className= "btn-large"
+                                onClick={this.clickHandler.bind(this)}>Restart</button>
+                        <br/>
+                        <br/>
+                        {/* <p>[place holders] (counts) clicks / [Win] Puzzle Solved in (counts) clicks</p> */}
             </div>
         );
     }
@@ -44,9 +53,86 @@ class Game extends React.Component{
             win: false
         }
     }
+    shuffle(array){
+            // switches first two tiles
+            function switchTiles(array) {
+                var i = 0;
+
+                // find the first two tiles in a row
+                while (!array[i] || !array[i+1]) i++;
+
+                // store tile value
+                var tile = array[i];
+                // switche values
+                array[i] = array[i+1];
+                array[i+1] = tile;
+
+                return array;
+            }
+            // counts
+            function countInversions(array) {
+                // make array of inversions
+                var invArray = array.map(function(num, i) {
+                    var inversions = 0;
+                    let j=0;
+                    for (j = i + 1; j < array.length; j++) {
+                        if (array[j] && array[j] < num) {
+                            inversions += 1;
+                        }
+                    }
+                    return inversions;
+                });
+                // return sum of inversions array
+                return invArray.reduce(function(a, b) {
+                    return a + b;
+                });
+            }
+
+            // fischer-yates shuffle algorithm
+            function fischerYates(array) {
+                var counter = array.length, temp, index;
+
+                // While there are elements in the array
+                while (counter > 0) {
+                    // Pick a random index
+                    index = Math.floor(Math.random() * counter);
+                    // Decrease counter by 1
+                    counter--;
+                    // And swap the last element with it
+                    temp = array[counter];
+                    array[counter] = array[index];
+                    array[index] = temp;
+                }
+
+                return array;
+            }
+
+            // Fischer-Yates shuffle
+            array = fischerYates(array);
+
+            // check for even number of inversions
+            if (countInversions(array) % 2 !== 0) {
+                // switch two tiles if odd
+                array = switchTiles(array);
+            }
+
+            return array;
+    }
+    restartGame(){
+        this.setState(this.getInitialState());
+    }
+    getInitialState(){
+        return {
+            // initial state of game board
+            tiles: this.shuffle([
+                1,2,3,
+                4,5,6,
+                7,8,''
+            ]),
+            win: false
+        };
+    }
     
-    // tileClick(){
-        // console.log('this.state.tiles',this.state.tiles)
     checkBoard(){
         let tiles = this.state.tiles;
         for (let i =0; i<tiles.length-1; i++){
@@ -141,7 +227,7 @@ class Game extends React.Component{
                             className="col s5 "
                             name="challenger"
                             type="text"
-                            placeholder="I am ..."
+                            placeholder="submit your name to join"
                             value={this.state.challenger}
                             />
                     <div className="col s1">  </div>
@@ -155,6 +241,7 @@ class Game extends React.Component{
             {/* GameForm End */}
             
             <div className="container center">
+                <p>{this.state.tiles}</p> 
                 <div id="game-board">
                 {/* {this.state.tiles.map(funciton( tile, position ) {
                     return(
@@ -169,10 +256,8 @@ class Game extends React.Component{
                 )}
                 </div>
             </div>
-
-
-            <p>{this.state.tiles}</p> 
-            <StatusMenu />
+            <StatusMenu tileClick={this.tileClick.bind(this)} 
+                        restart={this.restartGame.bind(this)}/>
             </div>
    
         )
